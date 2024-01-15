3ceb57c9a32244c3ddf77248b2b1c64636b0fe85
Adeel Imran <adeelimranr@gmail.com>
2024-01-15 12:30:39 +0100

    feat: add AI assitant

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,4 +1,5 @@
 import MoviesCarousel from "@/components/MoviesCarousel";
+import SuggestionsAI from "@/components/SuggestionsAI";
 import { getDiscoverMovies } from "@/lib/getMovies";
 import { notFound } from "next/navigation";
 
@@ -21,10 +22,9 @@ async function GenrePage({ params, searchParams }: GenrePageProps) {
 
   return (
     <div className="max-w-7xl mx-auto">
-      {/* @todo AI suggestions */}
       <div className="flex flex-col space-y-4 mt-32 xl:mt-42">
         <h1 className="text-6xl font-bold px-10">Results for {genre}</h1>
-
+        <SuggestionsAI term={genre} />
         <MoviesCarousel title="Genre" movies={movies} isVertical />
       </div>
     </div>
--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -1,4 +1,5 @@
 import MoviesCarousel from "@/components/MoviesCarousel";
+import SuggestionsAI from "@/components/SuggestionsAI";
 import { getPopularMovies, getSearchedMovies } from "@/lib/getMovies";
 import { notFound } from "next/navigation";
 
@@ -20,9 +21,7 @@ async function SearchPage({ params: { term: queryTerm } }: SearchPageProps) {
     <div className="max-w-7xl mx-auto">
       <div className="flex flex-col space-y-4 mt-32 xl:mt-42">
         <h1 className="text-6xl font-bold px-10">Results for {term}</h1>
-
-        {/* @todo AI suggestions */}
-
+        <SuggestionsAI term={term} />
         <MoviesCarousel title="Movies" movies={movies} isVertical />
         <MoviesCarousel title="You may also like" movies={popularMovies} />
       </div>
